0b2c39a5cad4b61d9f97ac888d282b039405a0ec
luciano.oliveira <luciano.oliveira@lynxprocess.com>
2024-03-19 16:02:37 -0300

    Feature: Create index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SSL PHP</title>
+</head>
+<body>
+    <h1>Hello, SSL!</h1>
+    <?php
+        // Imprime "Hello, World!" usando PHP
+        echo "<p>Olá, mundo!</p>";
+    ?>
+</body>
+</html>
